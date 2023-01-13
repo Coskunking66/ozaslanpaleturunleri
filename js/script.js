b89a730a546bd9640c078b439318e866c3d0520f
Coskunking66 <104674988+Coskunking66@users.noreply.github.com>
2023-01-13 09:26:29 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let baslik = document.title;
+		window.onblur = () =>
+		document.title= "Nereye Gittin :(";
+		window.onfocus=()=>
+		document.title=baslik;
+
+
+const navbar = document.querySelector(".navbar");
+
+//! buttons
+
+const menuBtn = document.querySelector("#menu-btn");
+
+
+menuBtn.addEventListener("click", function (){
+	navbar.classList.toggle("active");
+	document.addEventListener("click", function (e) {
+		if (
+			!e.composedPath().includes(menuBtn) &&
+			!e.composedPath().includes(navbar)
+		) {
+			navbar.classList.remove("active");
+		}
+	});
+});
+
+
+
+
+
+
